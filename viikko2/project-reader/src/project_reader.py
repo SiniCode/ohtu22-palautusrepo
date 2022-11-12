46f7eab3fab434c493da2d691d59b2b47eeb1a21
Sini Arkko <sini.arkko@gmail.com>
2022-11-12 14:41:53 +0200

    Complete project-reader task

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,22 @@
+from urllib import request
+from project import Project
+import toml
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        content_dict = toml.loads(content)
+        
+        name = content_dict["tool"]["poetry"]["name"]
+        description = content_dict["tool"]["poetry"]["description"]
+        dependencies = list(content_dict["tool"]["poetry"]["dependencies"])
+        dev_dependencies = list(content_dict["tool"]["poetry"]["dev-dependencies"])
+
+        return Project(name, description, dependencies, dev_dependencies)
