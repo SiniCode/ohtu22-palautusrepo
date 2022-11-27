73a23db6353f2ce847000770d0b55a1a25d716a4
Sini Arkko <sini.arkko@gmail.com>
2022-11-28 00:20:21 +0200

    Check that adding one item changes the price correctly

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,22 +3,26 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        self.ostokset_korissa = []
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
+        self.ostokset_korissa = []
 
     def tavaroita_korissa(self):
+        # kertoo korissa olevien tavaroiden lukumäärän
+        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2
+        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2
         total = 0
         for ostos in self.ostokset_korissa:
             total += ostos.lukumaara()
 
         return total
-        # kertoo korissa olevien tavaroiden lukumäärän
-        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2
-        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2
 
     def hinta(self):
-        return 0
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
+        total = 0
+        for ostos in self.ostokset_korissa:
+            total += ostos.hinta()
+
+        return total
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
