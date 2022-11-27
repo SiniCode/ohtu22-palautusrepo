d79e1ad1d9af87e7caa175bd91c2a167a02b6e36
Sini Arkko <sini.arkko@gmail.com>
2022-11-28 00:23:42 +0200

    Check that there are two items in the basket after adding two different products

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -21,3 +21,12 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
 
         self.assertEqual(self.kori.hinta(), 3)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorissa_on_kaksi_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        kaakao = Tuote("Kaakao", 6)
+
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(kaakao)
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
