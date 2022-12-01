868cf4f8a84eaacc01066c4657e93c5141efe4b8
Sini Arkko <sini.arkko@gmail.com>
2022-12-01 20:15:37 +0200

    Improve readability

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -2,56 +2,79 @@ class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player1_score = 0
+        self.player2_score = 0
 
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+        if player_name == self.player1_name:
+            self.player1_score = self.player1_score + 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.player2_score = self.player2_score + 1
+
+    def game_is_even(self):
+        return self.player1_score == self.player2_score
+
+    def get_even_score(self):
+        score = {
+            "0-0": "Love-All",
+            "1-1": "Fifteen-All",
+            "2-2": "Thirty-All",
+            "3-3": "Forty-All",
+            "4-4 or higher": "Deuce"
+        }
+
+        if self.player1_score == 0:
+            return score["0-0"]
+
+        if self.player1_score == 1:
+            return score["1-1"]
+
+        if self.player1_score == 2:
+            return score["2-2"]
+
+        if self.player1_score == 3:
+            return score["3-3"]
+
+        return score["4-4 or higher"]
+
+    def player1_is_winning_and_has_at_least_four_points(self):
+        return self.player1_score > self.player2_score and self.player1_score >= 4
+
+    def player2_is_winning_and_has_at_least_four_points(self):
+        return self.player2_score > self.player1_score and self.player2_score >= 4
+
+    def get_scores_under_four_points(self):
+        score = {
+            0: "Love",
+            1: "Fifteen",
+            2: "Thirty",
+            3: "Forty"
+        }
+
+        player1_score = score[self.player1_score]
+        player2_score = score[self.player2_score]
+
+        return f"{player1_score}-{player2_score}"
 
     def get_score(self):
-        score = ""
-        temp_score = 0
+        if self.game_is_even():
+            score = self.get_even_score()
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            elif self.m_score1 == 3:
-                score = "Forty-All"
+        elif self.player1_is_winning_and_has_at_least_four_points():
+            difference = self.player1_score - self.player2_score
+            if difference == 1:
+                score = f"Advantage {self.player1_name}"
             else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
+                score = f"Win for {self.player1_name}"
 
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
+        elif self.player2_is_winning_and_has_at_least_four_points():
+            difference = self.player2_score - self.player1_score
+            if difference == 1:
+                score = f"Advantage {self.player2_name}"
             else:
-                score = "Win for player2"
+                score = f"Win for {self.player2_name}"
+
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
-
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+            score = self.get_scores_under_four_points()
 
         return score
