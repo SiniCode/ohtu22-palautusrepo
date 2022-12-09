e7b937b874331a9d248556caac05e5d301e04288
Sini Arkko <sini.arkko@gmail.com>
2022-12-10 00:29:38 +0200

    Add query builder

---
+++ viikko6/query-language/src/query_builder.py
@@ -0,0 +1,18 @@
+from matchers import And, All, PlaysIn, HasAtLeast, HasFewerThan
+
+
+class QueryBuilder:
+    def __init__(self, query = All()):
+        self._query = query
+
+    def build(self):
+        return self._query
+
+    def playsIn(self, team):
+        return QueryBuilder(And(self._query, PlaysIn(team)))
+
+    def hasAtLeast(self, value, attr):
+        return QueryBuilder(And(self._query, HasAtLeast(value, attr)))
+
+    def hasFewerThan(self, value, attr):
+        return QueryBuilder(And(self._query, HasFewerThan(value, attr)))
